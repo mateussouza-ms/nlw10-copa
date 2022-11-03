a3e6a83d13911e6a81aa282342ec151b7d2d187e
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-11-03 13:46:20 -0300

    {sever] Add routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
-import Fastify from "fastify";
-
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+import ShortUniqueId from "short-unique-id";
+import z from "zod";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -24,8 +25,39 @@ async function start() {
     return { pools };
   });
 
+  fastify.post("/pools", async (request, response) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    const pool = await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return response.status(201).send({ pool });
+  });
+
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
 
     return { count };
   });
