76849af1ced7db4d0b933a245499540f517667cc
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-11-05 12:37:41 -0300

    [mobile] Create login page

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,24 @@
-import { Center, Text } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+import { Center, Icon, Text } from "native-base";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white">SignIn</Text>
+    <Center flex={1} bgColor="gray.900" p={7} w="full">
+      <Logo width={212} height={40} />
+      <Button
+        type="SECONDARY"
+        title="Entrar com o Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"}
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   );
 }
