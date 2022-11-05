ee7aba06075bfc120b3338ef8c0628dd8422c557
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-11-05 13:39:59 -0300

    [mobile] Implements Google oAuth authentication

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode, useState } from "react";
+import * as AuthSession from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export interface UserProps {
   name: string;
@@ -6,6 +11,7 @@ export interface UserProps {
 }
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -20,13 +26,42 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     name: "Mateus Souza",
     avatarUrl: "https://guthub.com/mateeussouza-ms.png",
   });
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "667464238359-nf93ldmin40atkubejeq3jv18hjulkuj.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({
+      useProxy: true,
+    }),
+    scopes: ["profile", "email"],
+  });
 
   async function signIn() {
-    console.log("TODO: Login...");
+    try {
+      setIsUserLoading(true);
+
+      await promptAsync();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
 
+  async function signInWitGoogle(access_token: string) {
+    console.log("access_token", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWitGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, isUserLoading, signIn }}>
       {children}
     </AuthContext.Provider>
   );
