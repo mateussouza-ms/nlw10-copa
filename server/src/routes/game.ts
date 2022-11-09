aab63b4b57ac273aa5802c93d5fb20a44fae8c8f
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-11-08 23:33:21 -0300

    [server] Create game and guess routes

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,7 +1,46 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/pools/:id/games",
+    { onRequest: [authenticate] },
+    async (request) => {
+      const getPoolGamesParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolGamesParams.parse(request.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                poolId: id,
+              },
+            },
+          },
+        },
+      });
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
+
   fastify.get("/games/count", async () => {
     const count = await prisma.game.count();
 
